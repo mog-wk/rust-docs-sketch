bc3b8bb472630b95c9a1b228425f7a08073a761d
mog-wk <ms-work@mail.com>
2023-05-25 10:36:16 -0300

    added usage function in cp 12 main

--- 12-grep-clone/src/main.rs
+++ 12-grep-clone/src/main.rs
@@ -9,12 +9,21 @@ fn main() {
 
     let config: Config = grepclone::Config::new(&args).unwrap_or_else(|err| {
         eprintln!("problem parsing arguments: {}", err);
+        usage();
         process::exit(1);
     });
 
     if let Err(e) = grepclone::run(config) {
         eprintln!("Application error {}", e);
+        usage();
         process::exit(1);
     }
 }
 
+fn usage() {
+    println!("\
+    \tUsange:
+    cargo run <file_path> <expression>\t\t=> sensitive search
+    CASE_INSENSITIVE=1 cargo run <file_path> <expression>\t\t=> insensitive search
+    ");
+}
